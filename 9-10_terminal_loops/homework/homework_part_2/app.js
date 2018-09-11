b5134cc199dd0237022fbbc6290353448e26a3d1
Colin <colinmdaniel@gamil.com>
2018-09-10 23:25:49 -0500

    homework part 2

---
+++ 9-10_terminal_loops/homework/homework_part_2/app.js
@@ -0,0 +1,48 @@
+/* i belive they sever the same purpouse
+interpolation ex: console.log(${varible_to_call} + " or " + other_varible);
+concatenation ex: print("this is " the_varible) or my_string % (var1, var2 ,var3)
+
+Dont Repeat yourself. keeps for cleaner code and not polluting the global scope.not sure
+
+declaring a varible doesnt mean it has a value it can be undefined. defining a variable means it has a value
+
+we use const when the varible is not going to be changed and let when we do.
+
+the directory that nests other files and or directories.
+
+--help
+
+not sure.
+*/
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+console.log(a < b < c);
+console.log(a == a < d);
+console.log(e === 'Kevin');
+console.log(48 == '48');
+
+// it is not inifinite
+/*it is finite becase runProgram is set to false as soon as it runs
+A x20 is logged because the loop runs until i is greater than or equal to 20
+
+they are the same you can just set while loops to stop
+running at a certain condition not just a number
+
+for(let i = 1; i < 1001; i++){
+	console.log([i]);
+}
+
+for(let i = 1000; i > -1; i--){
+	console.log([i]);
+}
+
+for(let i = 1; i < 11; i++){
+	console.log("the value of i is: [i] of 10);
+}
